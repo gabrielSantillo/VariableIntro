e22088f31b7a29d2e85c9081635ade4e6569d99a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-05 19:57:24 -0600

    Variable Intro

---
+++ app.js
@@ -0,0 +1,9 @@
+let givenName = "Gabriel";
+let familyName = "Santillo";
+let age = "26";
+let luckyNumber = "7";
+
+console.log(givenName);
+console.log(familyName);
+console.log(age);
+console.log(luckyNumber);
