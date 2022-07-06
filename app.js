5fdb82a26dd3771a31b14f7e0fa401301755b023
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-05 20:07:53 -0600

    Just noticed that my numbers were actually strings. Changed to number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let givenName = "Gabriel";
 let familyName = "Santillo";
-let age = "26";
-let luckyNumber = "7";
+let age = 26;
+let luckyNumber = 7;
 
 console.log(givenName);
 console.log(familyName);
