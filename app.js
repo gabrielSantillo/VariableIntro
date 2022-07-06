b8e32422c42f9b5727b8da2ab3f912d5b4bd9d02
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-05 20:35:08 -0600

    Added the conditionals to the file

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ console.log(givenName);
 console.log(familyName);
 console.log(age);
 console.log(luckyNumber);
+
+let user_points = 100;
+if (user_points >= 75) {
+    console.log("Congratulations!");
+}
